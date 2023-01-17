5414a47288218ef520630393f8f1ac40bcac82d3
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-01-17 09:32:46 +0000

    Deploying to gh-pages from @ yulyarudenko88/goit-js-hw-08@829097b85e951cc7ade9577fe6de6c78909ce468 🚀

--- 02-video.46a61dd3.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("vimeo-player"),t=new Player(e);t.on("timeupdate",lodash((e=>{localStorage.setItem("videoplayer-current-time",e.seconds),console.log(localStorage.getItem("videoplayer-current-time"))}),1e3)),localStorage.getItem("videoplayer-current-time")&&t.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
-//# sourceMappingURL=02-video.46a61dd3.js.map
